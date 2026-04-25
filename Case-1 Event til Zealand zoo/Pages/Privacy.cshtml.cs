7a684aff98ca1583bec9611d9ae6eb0989488e3a
LucasPKonrad <lucas@getwise.dk>
2026-04-25 23:23:38 +0200

    New project

---
+++ Case-1 Event til Zealand zoo/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Case_1_Event_til_Zealand_zoo.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
